53e66ced9634d7e63ddb31184526dbb215a6bb18
SahilGawade <sahilgawade828@gmail.com>
2025-02-27 11:39:07 +0530

    Create index2.php

---
+++ day1/01_welcome/index2.php
@@ -0,0 +1,23 @@
+<?php
+echo"
+<html>
+<head>
+<title>welcome web page</title>
+</head>
+<body>
+    <i>php in italic</i><br>
+    <b>php in bold</b><br>
+    <u>php in underline</u><br>
+    <em>php in empasized</em><br>
+    <strong>php in strong</strong><br>
+    <mark>php in highlihgt</mark><br>
+    <small>php in small</small><br>
+    <big>php in big</big><br>
+    <ins>php in insted</ins><br>
+    <del>php in delted</del><br>
+    <sup>php in supersript</sup><br>
+    <sub>php in subscript</sub><br>
+</body>
+</html>
+"
+?>
